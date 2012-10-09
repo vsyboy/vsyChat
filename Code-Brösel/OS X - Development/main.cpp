249c79a3a03a2526ce8dc2ed90edaaf9dcf3bd2a
Kai <cyantific@me.com>
2012-10-09 13:55:13 +0200

    v0.1
    Source code refreshed

--- Code-Brösel/OS X - Development/main.cpp
+++ Code-Brösel/OS X - Development/main.cpp
@@ -2,35 +2,34 @@
 #include <string>
 #include <mysql.h>
 
-using namespace std;
 
 
 
 int main() {
 
-std::vector<std::string> tables;
-MYSQL_RES *result;
-MYSQL_ROW row;
-MYSQL *connection, mysql;
-
-int state;
-
-mysql_init(&mysql);
-
-connection = mysql_real_connect(&mysql,"localhost","username","password","database",0,0,0);
-
-if (connection == NULL)
-{
-    std::cout << mysql_error(&mysql) << std::endl;
-    
-    return tables;
+    std::string tables;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+	MYSQL *connection, mysql;
+	
+	int state;
+	
+	mysql_init(&mysql);
+	
+	connection = mysql_real_connect(&mysql,"localhost","username","password","database",0,0,0);
+	
+	if (connection == NULL)
+	{
+		std::cout << mysql_error(&mysql) << std::endl;
+		
+		return 1;
 	}
 	
 	state = mysql_query(connection, "SHOW TABLES");
 	if (state !=0)
 	{
 		std::cout << mysql_error(connection) << std::endl;
-		return tables;
+		return 1;
 	}
 	
 	result = mysql_store_result(connection);
@@ -38,12 +37,11 @@ if (connection == NULL)
 	std::cout << "tables: " << mysql_num_rows(result) << std::endl;
 	while ( ( row=mysql_fetch_row(result)) != NULL )
 	{
-		tables.push_back(row[0]);
+	
 	}
 	
 	mysql_free_result(result);
 	
 	mysql_close(connection);
-
     return 0;
 }
